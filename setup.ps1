9e67503310f798d5752becc76c311851584b2db6
Anthony Watherston <anwather@microsoft.com>
2022-06-27 11:22:56 +1000

    Initial commit

---
+++ setup.ps1
@@ -0,0 +1,6 @@
+New-Item .\policies\definitions -ItemType Directory -Verbose
+New-Item .\policies\initiatives -ItemType Directory -Verbose
+New-Item .\policies\assignments -ItemType Directory -Verbose
+New-Item .\policies\assignmentDefinitions -ItemType Directory -Verbose
+
+New-Item .\policies\assignments\TopLevelManagementGroup -ItemType Directory -Verbose
